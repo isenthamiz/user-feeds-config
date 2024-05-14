8f397ba5ef63878aa15dbdb47095fab4f7b188a8
Senthamiz Kumaran <ithamiz@amazon.com>
2024-05-14 16:01:49 +0100

    remove dist directory

--- dist/Configuration/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getSomeConfig = void 0;
-const getSomeConfig = function () {
-    return "SOME_CONFIG";
-};
-exports.getSomeConfig = getSomeConfig;
